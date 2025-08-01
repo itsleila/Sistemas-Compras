c933657a82a6b271274e7d9f9f8a89eec6765486
leila <129291974+itsleila@users.noreply.github.com>
2025-08-01 19:36:58 -0300

    consertando rota do React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from './infra/firebase';
